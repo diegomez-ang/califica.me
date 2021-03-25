8fde1f7e86bb79c089df11d704a4e34739bd5b27
Diego Alejandro Gomez Angarita <diego.gomez@umb.edu.co>
2021-03-25 09:45:57 -0400

    pre entrega

--- notas.js
+++ notas.js
@@ -1,16 +1,23 @@
-document.getElementById('nuevoEstudiante').addEventListener('click', crearEstudiante)
-document.getElementById('asistencia').addEventListener('click', registroAsistencia)
+/* document.getElementById('nuevoEstudiante').addEventListener('click', crearEstudiante)
+document.getElementById('asistencia').addEventListener('click', registroAsistencia) */
+
+$('#nuevoEstudiante').click(crearEstudiante)
 
 function crearEstudiante() {
     //capturar dato
-    let nombres = document.getElementById('nombres').value
-    let identificacion = document.getElementById('identificacion').value
-    let email = document.getElementById('email').value
+
+    let nombres = $('#nombres').val()
+    let identificacion = $('#identificacion').val()
+    let email = $('#email').val()
+
+    console.log( $('#nombres').val())
+    console.log( $('#identificacion').val())
+    console.log( $('#email').val())
 
     let nodoNombre = document.createElement('tr')
     let nodoNombreTexto = document.createTextNode(nombres)
     nodoNombre.appendChild(nodoNombreTexto)
-    document.getElementById('listaNombre').appendChild(nodoNombre)
+    document.getElementById('listaNombre').appendChild(nodoNombre) 
 
     let nodoIdentificacion = document.createElement('tr')
     let nodoIdentificacionTexto = document.createTextNode(identificacion)
@@ -20,9 +27,14 @@ function crearEstudiante() {
     let nodoCorreo = document.createElement('tr')
     let nodocorreoTexto = document.createTextNode(email)
     nodoCorreo.appendChild(nodocorreoTexto)
-    document.getElementById('listaCorreo').appendChild(nodoCorreo)
+    document.getElementById('listaCorreo').appendChild(nodoCorreo) 
 
-    document.getElementById('nombres').value = ''
-    document.getElementById('identificacion').value = ''
-    document.getElementById('email').value = ''
+    $('#nombres').val('')
+    $('#identificacion').val('')
+    $('#email').val('') 
+    $('#notificacion').fadeIn()   
+    $('#listaNombre').fadeIn()
+    $('#listaIdentificacion').fadeIn()
+    $('#listaCorreo').fadeIn()
 }
+
